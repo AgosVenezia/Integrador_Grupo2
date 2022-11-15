4f71633e73da9cd739d3a19af9adc528bb571feb
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2022-11-15 10:01:32 -0300

    Models y front

---
+++ grupo2/choices.py
@@ -0,0 +1,38 @@
+sexos=(
+    ('F','Femenino'),
+    ('M','Masculino'),
+    ('N', 'No Binario'),
+    ('O', 'Otros')
+)
+
+categoriaSocio=(
+    ('C','Cadete'),
+    ('A','Activo'),
+    ('V','Vitalicio'),
+    ('H','Honorario'),
+    ('B','Baja'),
+    ('F','Fallecido'),
+    ('R','Renuncia')
+)
+
+categoriaDistintiva=(
+    ('N','Novicio'),
+    ('G','General'),
+    ('S','Superior'),
+    ('E','Especial')
+)
+
+condicionDePago=(
+    ('S','En Sede'),
+    ('C','Cobrador'),
+    ('L','En Linea')
+)
+
+estadoCivil=(
+    ('S','Soltero'),
+    ('C','Casado'),
+    ('D','Divorciado'),
+    ('B','Concubinado'),
+    ('U','Union Civil'),
+    ('V','Viudo')
+)
--- grupo2/models.py
+++ grupo2/models.py
@@ -1,30 +1,66 @@
 from email.policy import default
 from django.db import models
+from .choices import *
 
 # Create your models here.
 
+class ciudadResidencia (models.Model):  
+    idCiudad=models.IntegerField(max_length=3)
+    ciudad=models.CharField(max_length=100,verbose_name='Ciudad')
+    provincia=models.CharField(max_length=100,verbose_name='Provincia')
+    codigoPostal=models.IntegerField(max_length=4,verbose_name='Codigo Postal')
 
 class Socio(models.Model):
-    fechaAsociacion= models.DateField()
-    categoriaSocio= models.IntegerField(max_length=1)
-    distintiva=models.CharField(max_length=6)
-    categoriaDistintiva=models.IntegerField(max_length=1)
-    expediente = models.FileField(upload_to='expedientes/')
+    idSocio=models.IntegerField(max_length=4,primary_key=True,default=0000) # numero interno de referencia - PK
+    fechaAsociacion= models.DateField(verbose_name='Fecha de alta en al club',null=True) # fecha que ingreso al club
+    idCategoriaSocio=models.CharField(max_length=1, choices=categoriaSocio, default='A')  # Categoria de socio 0: 2:vitalicio 
+    distintiva=models.CharField(max_length=6,null=True) # Distintiva propia segun un convenio internacional
+    categoriaDistintiva=models.CharField(max_length=1, choices=categoriaDistintiva, default='N') # Categoria que corresponde a su distintiva
+    expediente = models.FileField(upload_to='expedientes/', null=True ) # archivo de referencia con contrado y seguro
     nombre = models.CharField(max_length=100,verbose_name='Nombre')
     apellido = models.CharField(max_length=100,verbose_name='Apellido')
-    estadoCivil=models.IntegerField(max_length=1)
-    sexo=models.CharField(max_length=1)
-    tipo_dni = models.CharField(max_length=10, default='dni')
-    dni = models.IntegerField(verbose_name='DNI')
-    cuil=models.IntegerField(max_length=11)
-    fechaNacimiento=models.DateField()
-    direccion=models.CharField(max_length=100,verbose_name='direccion')
-    ciudad=models.IntegerField(max_length=3)
-    email = models.EmailField(max_length=150)
+    estadoCivil=models.CharField(max_length=1, choices=estadoCivil, default='S')
+    sexo=models.CharField(max_length=1,choices=sexos, default='F') # M, F, N, 0,
+    tipo_dni = models.CharField(max_length=10, default='DNI')
+    dni = models.IntegerField(verbose_name='DNI',null=True)
+    cuil = models.IntegerField(max_length=11,null=True)
+    fechaNacimient = models.DateField(verbose_name='Fecha de nacimiento',null=True)
+    direccion = models.CharField(max_length=100,verbose_name='direccion',null=True)
+    ciudadResidencia=models.ForeignKey(ciudadResidencia, on_delete=models.CASCADE, null=True)
+    ocupacion=models.CharField(max_length=100,verbose_name='Ocupacion',null=True)
+    telDomicilio=models.IntegerField(max_length=15,null=True)
+    telCelular=models.IntegerField(max_length=15,null=True)
+    email = models.EmailField(max_length=150,null=True)
+    condicionDePago=models.CharField(max_length=1, choices=condicionDePago, default='S') # 0:en sede  1:pasa cobrador por le domicilio
+    pesentadoPor=models.IntegerField(max_length=4,null=True) # que socio lo presento
+    venciDistintiva=models.DateField(verbose_name='Vencimiento de la Distintiva',null=True)  # Vencimiento de la Distintiva
+
+    web=models.CharField(max_length=100,verbose_name='web',null=True)
+
+    
+class comprobante (models.Model): 
+    comprobante=models.IntegerField(max_length=8,primary_key=True)  # numero de comprobante en la factura
+    fecha= models.DateField(null=True) # fecha de pago
+    montoComprobante=models.FloatField(max_length=6,default=0,verbose_name='Monto Total')  # monto total del comprobante
+    observaciones=models.CharField(max_length=100,verbose_name='Observacion',null=True)  # opcional
+
+
+
+class cuota (models.Model):  
+    cuota=models.IntegerField(max_length=6,primary_key=True)  # numero de cuota  año+mes ej: 202003
+    socio=models.ForeignKey(Socio, on_delete=models.CASCADE, null=True)  # referencia con tabla socio a quien corresponde la cuota
+    comprobante=models.ForeignKey(comprobante, on_delete=models.CASCADE,null=True)  # referencia con el comprobante donde se abono la cuota
+    montoCuota=models.FloatField(max_length=6,default=0)  # monto de la cuota que se abono
+
+
+
+    
+
+
+
+      
     
     
-    
-    fechaVencimiento=models.DateField()
 
 
 
@@ -49,6 +85,7 @@ class Categoria(models.Model):
 class Curso(models.Model):
     nombre = models.CharField(max_length=100,verbose_name='Nombre')
     descripcion = models.TextField(null=True,verbose_name='Descripcion')
+    imagen= models.FileField(upload_to='../static/img/', null=True )
     #fecha_inicio = models.DateField(verbose_name='Fecha de Inicio')
     #portada = models.ImageField(upload_to='imagenes/',null=True,verbose_name='Portada')
     #many-to-one 
